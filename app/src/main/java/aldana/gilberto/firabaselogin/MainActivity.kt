0baabc49dd67f58e10b26d4b6fbe1cc3ddde769c
Gilberto Aldana <jesus.aldana@potros.itson.edu.mx>
2021-12-06 14:43:24 -0700

    Initial commit

---
+++ app/src/main/java/aldana/gilberto/firabaselogin/MainActivity.kt
@@ -0,0 +1,24 @@
+package aldana.gilberto.firabaselogin
+
+import aldana.gilberto.firabaselogin.databinding.ActivityMainBinding
+import aldana.gilberto.firabaselogin.databinding.ActivitySignInBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+}
