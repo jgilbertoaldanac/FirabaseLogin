a9b4c33d715a9b1186acfabc6f7138ee763c80bf
Gilberto Aldana <jesus.aldana@potros.itson.edu.mx>
2021-12-07 00:23:05 -0700

    Funcion

--- app/src/main/java/aldana/gilberto/firabaselogin/SignUpActivity.kt
+++ app/src/main/java/aldana/gilberto/firabaselogin/SignUpActivity.kt
@@ -13,7 +13,6 @@ import com.google.firebase.ktx.Firebase
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
-
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySignUpBinding
 
@@ -23,45 +22,46 @@ class SignUpActivity : AppCompatActivity() {
         binding= ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize Firebase Auth
         auth = Firebase.auth
 
-        binding.signUpButton.setOnClickListener{
-            val mEmail = binding.emailEditText.text.toString()
-            val mPassword = binding.emailEditText.text.toString()
+        binding.signUpButton.setOnClickListener {
+            val mEmail=binding.emailEditText.text.toString()
+            val mPassword=binding.passwordEditText.text.toString()
             val mRepeatPassword = binding.repeatPasswordEditText.text.toString()
-            val passwordRegex = Pattern.compile("^"+
-            "(?=.*[-@#$%^&+=])" + //Al menos 1 caractér especial
-            ".{6,}" + // Al menos 4 caracteres
-            "$" )
+            val passwordRegex = Pattern.compile("^" +
+                    "(?=.*[-@#$%^&+=])" +     // Al menos 1 carácter especial
+                    ".{6,}" +                // Al menos 4 caracteres
+                    "$")
 
-            if(mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()){
-                Toast.makeText(this, "Ingrese un email valido.", Toast.LENGTH_SHORT).show()
-            } else if(mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()){
-                Toast.makeText(this, "La contraseña es debil.", Toast.LENGTH_SHORT).show()
-            } else if(mPassword != mRepeatPassword){
-                Toast.makeText(this, "Confirma la contraseña.", Toast.LENGTH_SHORT).show()
-            }else{
+            if(mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+                Toast.makeText(this, "Ingrese un email valido.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()){
+                Toast.makeText(this, "La contraseña es debil.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mPassword != mRepeatPassword){
+                Toast.makeText(this, "Confirma la contraseña.",
+                    Toast.LENGTH_SHORT).show()
+            } else {
                 createAccount(mEmail, mPassword)
             }
-
         }
 
-        binding.backImageView.setOnClickListener{
-            val intent = Intent(this, SignInActivity::class.java)
+        binding.backImageView.setOnClickListener {
+            val intent= Intent(this,SignInActivity::class.java)
             startActivity(intent)
         }
-
     }
 
-    private fun createAccount(email: String, password: String){
+    private fun createAccount(email: String,password:String){
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-
+                    Toast.makeText(this, "Se ha registrado con exito",
+                        Toast.LENGTH_SHORT).show()
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.makeText(this, "No se pudo crear la cuenta. Vuelva a intertarlo",
                         Toast.LENGTH_SHORT).show()
                 }
             }
